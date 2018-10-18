333fa17cef9d8d444c24249a4b6cf82454e851da
Halil Ozercan <halilozercan@gmail.com>
2018-10-18 14:12:15 +0300

    Update HtmlTextView.java

--- HtmlTextView/src/main/java/org/sufficientlysecure/htmltextview/HtmlTextView.java
+++ HtmlTextView/src/main/java/org/sufficientlysecure/htmltextview/HtmlTextView.java
@@ -16,6 +16,7 @@
 
 package org.sufficientlysecure.htmltextview;
 
+import android.os.Build;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
